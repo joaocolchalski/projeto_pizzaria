068a1e15ddc7def4abaf724c467a01927dc2a18a
João Cleber Colchalski <joaocleber.jc10@gmail.com>
2025-05-01 22:09:51 -0300

    Começando o frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.scss";
+
+export const metadata: Metadata = {
+  title: "Sujeito Pizza - A Melhor Pizzaria",
+  description: "A melhor pizzaria do Brasil!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
